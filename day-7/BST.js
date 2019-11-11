0d36969593dbb282558548bd5c0bcb25088693bb
MohammadArsalan <a4arshi@yahoo.com>
2019-11-11 18:07:24 +0500

    add basic BST example

---
+++ day-7/BST.js
@@ -0,0 +1,46 @@
+function Node(data, left = null, right = null) {
+  this.data = data;
+  this.left = left;
+  this.right = right;
+}
+
+function BST() {
+  this.root = null;
+  this.insert = insert;
+  this.inOrder = inOrder;
+
+  function insert(data) {
+    let newNode = new Node(data);
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    }
+    var current = this.root;
+    var parent;
+    while (true) {
+      parent = current;
+      if (data < current.data) {
+        current = current.left;
+        if (current === null) {
+          parent.left = newNode;
+          break;
+        }
+      } else {
+        current = current.right;
+
+        if (current === null) {
+          parent.right = newNode;
+          break;
+        }
+      }
+    }
+  }
+
+}
+
+var bst = new BST();
+bst.insert(5);
+bst.insert(4);
+bst.insert(6);
+console.log(bst.root);
+
