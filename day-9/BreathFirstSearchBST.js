620641e4dcfcbde71fc9fae568f759faa6874e16
MohammadArsalan <a4arshi@yahoo.com>
2019-12-19 12:37:34 +0500

    add breathFirstSearch in BST

---
+++ day-9/BreathFirstSearchBST.js
@@ -0,0 +1,81 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue (data) {
+    this.queue.push(data);
+  }
+
+  dequeue () {
+    return this.queue.shift();
+  }
+
+  isEmpty () {
+    return this.queue.length === 0;
+  }
+}
+
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert (key) {
+    if (this.root === null) {
+      return this.root = new Node(key)
+    }
+
+    this.insertNode(this.root, key)
+  }
+
+  insertNode (node, key) {
+    if (node.key > key) {
+      if (node.left === null) {
+        node.left = new Node(key);
+      } else {
+        this.insertNode(node.left, key)
+      }
+    } else {
+      if (node.right === null) {
+        node.right = new Node(key);
+      } else {
+        this.insertNode(node.right, key)
+      }
+    }
+  }
+
+  breathFirstSearch () {
+    let queue = new Queue();
+    queue.enqueue(this.root);
+
+    while (!queue.isEmpty()) {
+      let current = queue.dequeue();
+      console.log(current.key)
+      if (current.left !== null) {
+        queue.enqueue(current.left);
+      }
+      if (current.right !== null) {
+        queue.enqueue(current.right)
+      }
+    }
+  }
+}
+
+const bst = new BST();
+bst.insert(2);
+bst.insert(2);
+bst.insert(0)
+bst.insert(3)
+bst.insert(4)
+bst.insert(5)
+console.log(JSON.stringify(bst.root, undefined, 4))
+console.log(bst.breathFirstSearch())
