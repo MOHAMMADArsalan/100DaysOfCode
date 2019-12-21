eef4dcc17260d94f097450bfd4120d13347c6554
MohammadArsalan <a4arshi@yahoo.com>
2019-12-21 11:52:24 +0500

    check has Path sum from root node to leaf node

--- day-9/hasPathSum.js
+++ day-9/hasPathSum.js
@@ -1,22 +1,55 @@
+
+/**
+ * check every node till leaf node which sum up for specific value or not;
+ *
+ */
 function hasPathSum (node, sum) {
+  // check it leaf node
   if (node.left === null && node.right === null) {
-    return sum === node.key
+    return sum === node.key;
   }
 
-  let subSum = sum - node.key;
-  if (node.left !== null) {
-    let hasPath = this.hasPathSum(node.left, subSum);
+  let subSum = sum - node.key; // subtract current node value from sum
+
+  if (node.left !== null) { // if left node is not null find sum in left node
+    let hasPath = this.hasPathSum(node.left, subSum); // recusive call hasPathSum with left node and subSum
     if (hasPath) {
       return true
     }
   }
 
-  if (node.right !== null) {
-    let hasPath = this.hasPathSum(node.right, subSum);
+  if (node.right !== null) {// if right node is not null find sum in right node
+    let hasPath = this.hasPathSum(node.right, subSum);// recusive call hasPathSum with right node and subSum
     if (hasPath) {
       return true
     }
   }
 
-  return false
+  return false // return false if none of above condition run.
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
